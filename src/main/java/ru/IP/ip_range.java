be74a0d2031848a10f940303d658a19fbed91bfd
Olegjan-92 <Olegjan-92@mail.ru>
2016-10-10 16:38:25 +0300

    firststart

--- src/main/java/ru/IP/ip_range.java
+++ src/main/java/ru/IP/ip_range.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
  */
 public class ip_range {
     public static void main(String[] args) {
-        int lastNumOfIpStart = 0;
-        int lastNumOfIpEnd = 0;
+        int lastNumOfIpStart = 0; // последнее число начального адреса
+        int lastNumOfIpEnd = 0;   // последнее число конечного адреса
 
         Scanner scanner = new Scanner(System.in);           //
         System.out.println("Введите начальный ip адрес:");  //
@@ -16,17 +16,17 @@ public class ip_range {
         System.out.println("Введите конечный ip адрес:");   //
         String ipEnd= scanner.next();                       //
 
-        String[] NumOfIpStart = ipStart.split("\\.");
-        String[] NumOfIpEnd = ipEnd.split("\\.");
+        String[] NumOfIpStart = ipStart.split("\\.");  //разделение на подмассывы
+        String[] NumOfIpEnd = ipEnd.split("\\.");      //
 
-        try {
-            lastNumOfIpStart = Integer.valueOf(NumOfIpStart[NumOfIpStart.length-1]);
-            lastNumOfIpEnd = Integer.valueOf(NumOfIpEnd[NumOfIpEnd.length-1]);
-        }catch (NumberFormatException e){
-            System.err.println("Неверный формат строки");
+        try {                                                                           //
+            lastNumOfIpStart = Integer.valueOf(NumOfIpStart[NumOfIpStart.length-1]);    //
+            lastNumOfIpEnd = Integer.valueOf(NumOfIpEnd[NumOfIpEnd.length-1]);          //перевод в int последних чисел адресов
+        }catch (NumberFormatException e){                                               //
+            System.err.println("Неверный формат строки");                               //
         }
 
-        for (int i = lastNumOfIpStart+1; i<lastNumOfIpEnd; i++){
+        for (int i = lastNumOfIpStart+1; i<lastNumOfIpEnd; i++){                              
             System.out.printf("%s.%s.%s.%s",NumOfIpStart[0],NumOfIpStart[1],NumOfIpStart[2],i);
             System.out.println();
         }
